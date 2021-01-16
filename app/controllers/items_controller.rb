71a9bde0602328034bceff27e56cccbc05f2a93d
t-st7 <76399553+t-st7@users.noreply.github.com>
2021-01-16 22:53:59 +0900

    ActiveStrage導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,9 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  private
+  def item_params
+    require.params(:item).permit(:product, :description, :category_id, :delivery_charge_id, :state_id, :condition_id, :days_id, :price, :image).merge(user_id: current_user.id)
+  end
 end
