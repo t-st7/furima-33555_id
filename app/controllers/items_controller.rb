f00ac802ed2279b86f91b1be241a0be07260f962
t-st7 <76399553+t-st7@users.noreply.github.com>
2021-01-17 11:52:58 +0900

    出品ページ移動、商品のバリデーション設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,22 @@
 class ItemsController < ApplicationController
+  before_action :move_to_sign_in, only: [:new]
   def index
+  end
+
+  def new
+  end
+
+  def create
   end
 
   private
   def item_params
     require.params(:item).permit(:product, :description, :category_id, :delivery_charge_id, :state_id, :condition_id, :days_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_sign_in
+    unless user_signed_in? 
+      redirect_to user_session_path
+    end
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,12 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  with_options presence: true do
+    validates [:product, :description, :category_id, :delivery_charge_id, :state_id, :condition_id, :days_id, :image]
+  end
+
+  with_options presence: true, format: {with: /\A[0-9]+\z/
+    } do
+    validates :price
+  end
 end
